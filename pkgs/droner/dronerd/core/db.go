21719d2d35deab8c7719dcf49a020d16acda47b7
oudwins <tm@tristanmayo.com>
2026-02-01 16:23:09 +0100

    wp

---
+++ pkgs/droner/dronerd/core/db.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+
+	"github.com/Oudwins/droner/pkgs/droner/dronerd/core/db"
+	"github.com/Oudwins/droner/pkgs/droner/internals/conf"
+
+	_ "modernc.org/sqlite"
+)
+
+func InitDB(config *conf.Config) (*db.Queries, error) {
+	dbPath := filepath.Join(config.Server.DataDir, "db", "droner.db")
+	if err := os.MkdirAll(filepath.Dir(dbPath), 0o755); err != nil {
+		return nil, err
+	}
+
+	conn, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := conn.Exec("PRAGMA journal_mode = WAL;"); err != nil {
+		return nil, err
+	}
+	if _, err := conn.Exec("PRAGMA synchronous = NORMAL;"); err != nil {
+		return nil, err
+	}
+
+	schemaPath := filepath.Join("dronerd", "core", "db", "schemas", "sessions.sql")
+	schema, err := os.ReadFile(schemaPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := conn.Exec(string(schema)); err != nil {
+		return nil, err
+	}
+
+	return db.New(conn), nil
+}
