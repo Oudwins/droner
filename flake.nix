2bf029155e0a2c66a2b200a81c4defb855103ac0
oudwins <tm@tristanmayo.com>
2026-02-01 15:40:15 +0100

    wp db

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ go gopls git gnumake psmisc ];
+          packages = with pkgs; [ go gopls git gnumake psmisc sqlc ];
         };
       });
 }
