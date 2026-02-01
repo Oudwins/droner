f4f485d675309818ae0b72913c19772cbc007596
oudwins <tm@tristanmayo.com>
2026-02-01 09:52:30 +0100

    refactor: schemas

--- pkgs/droner/internals/schemas/session.go
+++ pkgs/droner/internals/schemas/session.go
@@ -21,32 +21,7 @@ type SessionCreateRequest struct {
 }
 
 var SessionCreateSchema = z.Struct(z.Shape{
-	"Path": z.String().Required().Trim().Transform(func(valPtr *string, ctx z.Ctx) error {
-		*valPtr = filepath.Clean(*valPtr)
-		return nil
-	},
-	).TestFunc(func(valPtr *string, ctx z.Ctx) bool {
-		w, ok := ctx.Get("workspace").(workspace.Host)
-		if !ok {
-			ctx.AddIssue(ctx.Issue().SetMessage("Something wen't wrong trying to get workspace from context. Internal error"))
-			return true
-		}
-		file, err := w.Stat(*valPtr)
-		if err != nil {
-			ctx.AddIssue(ctx.Issue().SetMessage("Failed to stat path"))
-			return true
-		}
-
-		if !file.IsDir() {
-			ctx.AddIssue(ctx.Issue().SetMessage("Path is not a directory"))
-			return true
-		}
-		err = w.GitIsInsideWorkTree(*valPtr)
-		if err != nil {
-			ctx.AddIssue(ctx.Issue().SetMessage("Path is not to a git repo").SetError(err))
-		}
-		return true
-	}, z.Message("Path is not a git repo")),
+	"Path":      z.String().Required().Trim().Transform(cleanPathTransform).TestFunc(isGitRepoTest, z.Message("Path is not a git repo")),
 	"SessionID": z.String().Optional().Trim(),
 	"Agent": z.Ptr(z.Struct(z.Shape{
 		"Model":  z.String().Default(conf.GetConfig().Agent.DefaultModel).Trim(),
@@ -71,9 +46,38 @@ type SessionDeleteResponse struct {
 }
 
 var SessionDeleteSchema = z.Struct(z.Shape{
-	"Path":      z.String().Optional().Trim(),
+	"Path":      z.String().Optional().Trim().Transform(cleanPathTransform).TestFunc(isGitRepoTest, z.Message("Is not a valid git repo")),
 	"SessionID": z.String().Optional().Trim(),
 }).TestFunc(func(valPtr any, ctx z.Ctx) bool {
 	v := valPtr.(*SessionDeleteRequest)
 	return v.Path != "" || v.SessionID != ""
 }, z.Message("At least one of path or sessionId are required"))
+
+func cleanPathTransform(valPtr *string, c z.Ctx) error {
+	*valPtr = filepath.Clean(*valPtr)
+	return nil
+}
+
+func isGitRepoTest(valPtr *string, ctx z.Ctx) bool {
+	w, ok := ctx.Get("workspace").(workspace.Host)
+	if !ok {
+		ctx.AddIssue(ctx.Issue().SetMessage("Something wen't wrong trying to get workspace from context. Internal error"))
+		return true
+	}
+	file, err := w.Stat(*valPtr)
+	if err != nil {
+		ctx.AddIssue(ctx.Issue().SetMessage("Failed to stat path"))
+		return true
+	}
+
+	if !file.IsDir() {
+		ctx.AddIssue(ctx.Issue().SetMessage("Path is not a directory"))
+		return true
+	}
+	err = w.GitIsInsideWorkTree(*valPtr)
+	if err != nil {
+		ctx.AddIssue(ctx.Issue().SetMessage("Path is not to a git repo").SetError(err))
+	}
+	return true
+
+}
