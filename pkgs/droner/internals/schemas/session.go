bf23cc68aa25ec01080f3cbc0485f48e7057f09f
oudwins <tm@tristanmayo.com>
2026-02-03 08:32:36 +0100

    refactor: simple session ids

--- pkgs/droner/internals/schemas/session.go
+++ pkgs/droner/internals/schemas/session.go
@@ -3,12 +3,42 @@ package schemas
 import (
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/Oudwins/droner/pkgs/droner/internals/conf"
 	"github.com/Oudwins/droner/pkgs/droner/internals/workspace"
 
 	z "github.com/Oudwins/zog"
 )
+
+const (
+	SimpleSessionDelimiter = ".."
+)
+
+func NewSSessionID(s string) SSessionID {
+	return SSessionID(strings.ReplaceAll(s, ".", "/"))
+}
+
+// Simple Session ID
+type SSessionID string
+
+func sessionID() *z.StringSchema[SSessionID] {
+	return z.StringLike[SSessionID]()
+}
+
+func (i SSessionID) String() string {
+	return string(i)
+}
+
+// file system safe version of simpleID
+func (i SSessionID) FSsafe() string {
+	return strings.ReplaceAll(string(i), "/", ".")
+}
+
+// Folder name for the sesssion
+func (i SSessionID) SessionWorktreeName(repoName string) string {
+	return repoName + SimpleSessionDelimiter + string(i)
+}
 
 type SessionAgentConfig struct {
 	Model  string `json:"model" zog:"model"`
@@ -17,7 +47,7 @@ type SessionAgentConfig struct {
 
 type SessionCreateRequest struct {
 	Path      string              `json:"path"`
-	SessionID string              `json:"sessionId,omitempty" zog:"sessionId"`
+	SessionID SSessionID          `json:"sessionId,omitempty" zog:"sessionId"`
 	Agent     *SessionAgentConfig `json:"agent,omitempty"`
 }
 
@@ -26,7 +56,7 @@ var multiupleSlashes = regexp.MustCompile(`//+`)
 
 var SessionCreateSchema = z.Struct(z.Shape{
 	"Path":      z.String().Required().Trim().Transform(cleanPathTransform).TestFunc(isGitRepoTest, z.Message("Path is not a git repo")),
-	"SessionID": z.String().Optional().Trim().Match(sessionIDRegex).Not().Match(multiupleSlashes),
+	"SessionID": sessionID().Optional().Trim().Match(sessionIDRegex).Not().Match(multiupleSlashes),
 	"Agent": z.Ptr(z.Struct(z.Shape{
 		"Model":  z.String().Default(conf.GetConfig().Agent.DefaultModel).Trim(),
 		"Prompt": z.String().Optional().Trim(),
@@ -34,23 +64,23 @@ var SessionCreateSchema = z.Struct(z.Shape{
 })
 
 type SessionCreateResponse struct {
-	SessionID    string `json:"sessionId"`
-	SimpleID     string `json:"simpleId"`
-	WorktreePath string `json:"worktreePath"`
-	TaskID       string `json:"taskId"`
+	SessionID    SSessionID `json:"sessionId"`
+	SimpleID     string     `json:"simpleId"`
+	WorktreePath string     `json:"worktreePath"`
+	TaskID       string     `json:"taskId"`
 }
 
 type SessionDeleteRequest struct {
-	SessionID string `json:"sessionId" zog:"sessionId"`
+	SessionID SSessionID `json:"sessionId" zog:"sessionId"`
 }
 
 type SessionDeleteResponse struct {
-	SessionID string `json:"sessionId"`
-	TaskId    string `json:"taskId"`
+	SessionID SSessionID `json:"sessionId"`
+	TaskId    string     `json:"taskId"`
 }
 
 var SessionDeleteSchema = z.Struct(z.Shape{
-	"SessionID": z.String().Optional().Trim(),
+	"SessionID": sessionID().Optional().Trim(),
 })
 
 func cleanPathTransform(valPtr *string, c z.Ctx) error {
